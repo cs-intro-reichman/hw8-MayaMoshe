41f50217a0241d1708479252f30578115ac9179e
Maya Moshe <maya.moshe@post.runi.ac.il>
2026-01-04 14:11:20 +0200

    1.2

--- Network.java
+++ Network.java
@@ -139,7 +139,7 @@ public class Network {
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
        //// Replace the following statement with your code
-       String result= "Network:\\n";
+       String result= "Network:\n";
        for(int i=0;i<userCount;i++){
         result +=users[i].toString()+"\n";
        }
